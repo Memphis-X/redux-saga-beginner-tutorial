4d705873579589a7871b390b2ce0bfbf6240578f
Jordan <thememphiss@icloud.com>
2022-10-01 00:12:20 +0300

    Added Redux Saga Logic to the application

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 import "@babel/polyfill"
-
+import createSagaMiddleware from "redux-saga";
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-
+import { createStore, applyMiddleware } from "redux";
 import Counter from './Counter'
 import reducer from './reducers'
+import rootSaga from "./sagas"
+import { helloSaga } from "./sagas"
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(rootSaga)
 
-const action = type => store.dispatch({type})
+
+const action = type => store.dispatch({ type })
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')} 
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
     document.getElementById('root')
   )
 }
